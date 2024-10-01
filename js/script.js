a37ab87c821f13645915623ff3785fd1341992a2
jefrinjui <jefrin.jui.dev@gmail.com>
2024-10-01 23:43:16 +0600

    if & else

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 let input=document.querySelector(".input")
 let button=document.querySelector(".button")
 let result=document.querySelector(".result")
-let error=doc.querySelector(".error")
+let error=document.querySelector(".error")
 
 button.addEventListener('click' , ()=>{
     if(input.value==''){
-        error.innerHTML="Plese enter amy text"
+        error.innerHTML="Plese enter any text"
+        // error.innerHTML=''
     } 
+    else{
+        error.innerHTML=''
+        console.log(input.value)
+        result.innerHTML=input.value
+        input.value=''
+    }
 })
